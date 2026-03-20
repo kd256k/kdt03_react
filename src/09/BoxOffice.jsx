edcdbe64c0d6e81ad1622018e077438484758431
kd256k <ceyeye33@gmail.com>
2026-03-20 22:51:26 +0900

    style: remove dark mode from date input

--- src/09/BoxOffice.jsx
+++ src/09/BoxOffice.jsx
@@ -122,9 +122,7 @@ export default function BoxOffice() {
             </h1>
             <div className="w-9/10 flex justify-end">
                 <input className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg mb-4
-                                    focus:ring-blue-500 focus:border-blue-500 block ps-10 p-2.5
-                                    dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400
-                                     dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                    focus:ring-blue-500 focus:border-blue-500 block ps-10 p-2.5"
                         type="date" 
                         max={getYesterday()} 
                         value={selectedDt} 
